049b3c276e38c96fdabce05f39ce9c953c571715
Obianuju-Sunday <obianujusunday43@gmail.com>
2022-11-17 15:49:25 +0100

    Latest Sharent Website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: EstateAgency - v4.9.1
-* Template URL: https://bootstrapmade.com/real-estate-agency-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
